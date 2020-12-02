49563aa675d5a65f0361964cb62b7e2dd3d5791b
nathalier <nathalier@gmail.com>
2020-12-02 15:25:37 +0000

    Make a check if a person is from Manchester case insensitive

--- src/main/java/com/techreturners/exercise002/Exercise002.java
+++ src/main/java/com/techreturners/exercise002/Exercise002.java
@@ -3,7 +3,7 @@ package com.techreturners.exercise002;
 public class Exercise002 {
 
     public boolean isFromManchester(Person person) {
-        return person.getCity().equals("Manchester");
+        return person.getCity().equalsIgnoreCase("Manchester");
     }
 
     public boolean canWatchFilm(Person person, int ageLimit) {
